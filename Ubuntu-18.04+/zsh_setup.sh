7b3bc5b6e106d25174417ca85f58b6827f01b177
Abdul Jabbar <abdulabase@gmail.com>
2020-05-06 22:32:03 +0100

    2 step process

---
+++ Ubuntu-18.04+/zsh_setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+### SCRIPT VARIABLES ###
+ZSH_THEME=ys
+
+### SCRIPT LOGIC ###
+# Install zsh
+apt install zsh -y
+
+# Install Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" “” --unattended
+# Note: --unattended does not run change default shell script from installer, but default was already set to zsh on user creation
+
+# Change zsh theme
+sed -i "s:robbyrussel:${ZSH_THEME}:" .zshrc
+
+echo "Script Ended"
