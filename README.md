7d71f3c5421421018798ee3c0ae39fa1d45ad256
Abdul Jabbar <abdulabase@gmail.com>
2020-05-06 20:54:24 +0100

    update for my system

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Automated Setups
+
+This setup assumed you already have an ssh key stored in you Digital Ocean account, and this key is selected when creating a new droplet
---
+++ Ubuntu-18.04+/initial_server_setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+### SCRIPT VARIABLES ###
+# USERNAME=abduljabbar
+# ZSH_THEME=ys
+
+
+### SCRIPT LOGIC ###
+# Install zsh
+apt install zsh -y
+
+# Add user, with same named group, passing blank gecos values
+# adduser --disabled-password --gecos "" --shell /usr/bin/zsh "${USERNAME}" //
+adduser --gecos "" --shell /usr/bin/zsh "${USERNAME}"
+
+# Add user to 'sudo' group
+usermod -aG sudo "${USERNAME}"
+
+# Copy `authorized_keys` file from root with approriate permissions
+rsync --archive --chown="${USERNAME}":"${USERNAME}" ~/.ssh /home/"${USERNAME}"
+
+# Change Oh My Zsh install directory to new user's home
+export ZSH=/home/“${USERNAME}”
+
+# Install Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" “” --unattended
+# Note: --unattended does not run change default shell script from installer, but default was already set to zsh on user creation
+
+# Change zsh theme
+sed -i ’s/robbyrussell/"${ZSH_THEME}"/g’ /home/"${USERNAME}".zshrc
+
+
+# Add exception for SSH and then enable UFW firewall
+ufw allow OpenSSH
+ufw --force enable # --force prevents user prompt
+
+echo "Script Ended"
